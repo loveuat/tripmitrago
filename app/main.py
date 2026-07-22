d5d053c630b4fc576b581b7b10d9feb78b3ddff1
loveuat <atulbramhe@gmail.com>
2026-07-22 13:12:50 +0530

    Worked on booking form

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://tripmitrago.onrender.com",
+        "https://ridenova-chi.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
